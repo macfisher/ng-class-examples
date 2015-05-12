b97d3477d8247f8738f32068a5faeaac0473a0e4
Mac Fisher <macfisher01@gmail.com>
2015-05-12 16:18:58 -0600

    added more examples

--- app.js
+++ app.js
@@ -3,4 +3,22 @@ var ngClassExample = angular.module('ngClassExample', []);
 ngClassExample.controller('mainController', ['$scope', function($scope) {
 	
 	$scope.message = "Hello world!";
+	
+	$scope.items = [
+		{
+			name : 'terminal',
+			description : 'flickery',
+			interact : 'true'
+		},
+		{
+			name : 'robot',
+			description : 'lame',
+			interaction : false
+		},
+		{
+			name : 'medical kit',
+			description : 'clean',
+			interaction : true
+		}
+	];
 }]);
